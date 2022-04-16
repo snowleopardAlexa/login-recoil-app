ec7b137e740baa49202cf9318c7484c4f49457ef
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-15 22:25:02 -0400

    import atom and useRecoilState hook which is like useState hook, set two variables repos and setRepos just like with useState hook, get response from json API request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { useEffect } from 'react'
+import { atom, useRecoilState } from 'recoil'
+
+// useRecoilState is like useState hook
+const reposState = atom({
+    key: "repos", // unique ID
+    default: []
+})
 
 const App = () => {
+// set two variables repos and setRepos just like with useState hook 
+const [repos, setRepos] = useRecoilState(reposState)   
 
 useEffect(() => {
     const getRepos = async() => {
         const url = "https:ghapi.huchen.dev/repositories?since=monthly"
         const resp = await fetch(url)
         const body = await resp.json()
-        console.log(body)
+        // get response from json API request
+        setRepos(body)
     }
 
     getRepos()
